f9e8be8a205d8cf4bd2d2d13af36ce7c13fada65
ioo5959 <ioo22@pukyong.ac.kr>
2024-08-02 14:37:47 +0900

    상품목록페이지

--- shooking/src/App.js
+++ shooking/src/App.js
@@ -1,21 +1,38 @@
 import React from 'react';
 import './App.css';
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
-import ProductList from './ProductList'; //상품 목록 페이지
-import CardPayment from './CardPayment';
+import sampleImage from './sampleImage.jpg'; // 이미지를 프로젝트에 추가하고 경로를 맞추기
 
 
 
 function App() {
-
-  return (
-    <Router>
-      <Routes>
-        <Route path="/" exact component={ProductList} />
-        <Route path="/payment" component={CardPayment} />
-      </Routes>
-    </Router>
-  );
-}
+  
+    return (
+      <div>
+        <header>
+          <nav style={{ backgroundColor: 'black', height: '50px',
+             display: 'flex', justifyContent: 'flex-end', alignItems: 'center', padding: '0 20px' }}>
+          </nav>
+        </header>
+        <main>
+          <h1>신발 상품 목록</h1>
+          <div className="content-box">
+          <div className="image-container">
+          <img src={sampleImage} alt="Sample" />
+          </div>
+          <div className="text-container">
+          <p>
+            브랜드 A
+            <br />
+            <span style = {{color: 'gray'}}>편안하고 착용감이 좋은 신발</span>
+            <br />
+            30000원
+          </p>
+        </div>
+        <button>담기</button>
+      </div>
+        </main>
+      </div>
+    );
+  }
 
 export default App;
